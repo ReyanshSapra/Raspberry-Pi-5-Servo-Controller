af2a72b8ae6ced76536b0310276a407424b11e86
Reyansh Sapra <168302081+ReyanshSapra@users.noreply.github.com>
2025-05-28 10:57:01 +0100

    Add files via upload

---
+++ servominio.py
@@ -0,0 +1,56 @@
+import streamlit as st
+from minio import Minio
+from minio.error import S3Error
+import io
+from io import BytesIO
+import time
+
+
+client = Minio(
+    endpoint="play.min.io",
+        access_key="Q3AM3UQ867SPQQA43P2F",
+        secret_key="zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+        secure=True
+    )
+
+uploadbool = False
+client.remove_object("raspberrypi5servocontrollerreyansh", "servoCLOSE")
+    
+if st.button("Open Servo"):
+    uploadbool = True
+
+if st.button("Close Servo"):
+    uploadbool = False
+
+if uploadbool == True:
+    if client.bucket_exists("raspberrypi5servocontrollerreyansh"):
+        client.put_object("raspberrypi5servocontrollerreyansh", "servoMOVE", io.BytesIO(b""), length=0)
+
+    else:
+        client.make_bucket("raspberrypi5servocontrollerreyansh")
+        client.put_object("raspberrypi5servocontrollerreyansh", "servoMOVE", io.BytesIO(b""), length=0)
+
+    client.remove_object("raspberrypi5servocontrollerreyansh", "servoCLOSE")
+    
+
+if uploadbool == False:
+    if client.bucket_exists("raspberrypi5servocontrollerreyansh"):
+        client.put_object("raspberrypi5servocontrollerreyansh", "servoCLOSE", io.BytesIO(b""), length=0)
+
+    else:
+        client.make_bucket("raspberrypi5servocontrollerreyansh")
+        client.put_object("raspberrypi5servocontrollerreyansh", "servoCLOSE", io.BytesIO(b""), length=0)
+
+    client.remove_object("raspberrypi5servocontrollerreyansh", "servoMOVE")
+        
+
+objects = client.list_objects("raspberrypi5servocontrollerreyansh", recursive=True)
+
+print(f"\nFile names in bucket:")
+found_objects = False
+for obj in objects:
+    print(f"  - {obj.object_name}") # Only printing the object name now
+    found_objects = True
+
+if not found_objects:
+    print(f"  No files found in bucket.")
